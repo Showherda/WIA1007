e4db2aee5b0bf5933245ca843735b76ed987cda8
Showherda <showherda@gmail.com>
2021-12-23 15:51:33 +0800

    Update script.r

--- script.r
+++ script.r
@@ -6,7 +6,6 @@ sitemap <- "https://www.coursera.org/sitemap~www~courses.xml"
 page <- read_html(sitemap)
 
 course_links <- str_split(str_extract_all(page, "(?<=<loc>).*?(?=</loc>)")[[1]], " ")
-#print((course_links[[6064]]))
 
 for (link in course_links){
   html <- read_html(link)
