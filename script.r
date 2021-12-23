1adce112f9c4505082617c03c69ae400e32a0893
Showherda <showherda@gmail.com>
2021-12-23 14:53:38 +0800

    Update script.r

--- script.r
+++ script.r
@@ -6,4 +6,11 @@ sitemap <- "https://www.coursera.org/sitemap~www~courses.xml"
 page <- read_html(sitemap)
 
 course_links <- str_split(str_extract_all(page, "(?<=<loc>).*?(?=</loc>)")[[1]], " ")
-print((course_links[[6064]]))
+#print((course_links[[6064]]))
+
+for (link in course_links){
+  html <- read_html(link)
+  name <- html_element(html, css = ".banner-title-without--subtitle") %>% html_text()
+  
+  print(name)
+}
