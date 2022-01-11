618dc3f0f3eaeee383c758f5885b1c28c5542f01
Showherda <showherda@gmail.com>
2022-01-11 19:33:56 +0800

    Update edx_scraper.r

--- edx_scraper.r
+++ edx_scraper.r
@@ -11,10 +11,13 @@ for (link in course_links){
   html <- read_html(link)
   name <- html_element(html, css = ".pr-4 h1") %>% html_text()
   level <- html_element(html, css = ".ml-1 > li:nth-child(3)") %>% html_text() %>% substr(9, 10000)
+  v <-substr(html_text(html_element(html, css = ".pb-md-0 .small")),3,3)
+  v <- if (is.na(v)) "1" else v
+  hrs_to_complt <- as.numeric(str_extract(html, "(?<=Estimated ).*?(?= weeks)"))*as.numeric(v)
   # ratings <- html_element(html, css = "div.rc-ReviewsOverview__totals__rating") %>% html_text()
   free_enrol <- if (is.na(html_element(html, css = ".track-headers-row+ .tr .td+ .td .comparison-item") %>% html_text())) F else T
   fin_aid <- T
-  x = paste(name, level, free_enrol, fin_aid, sep=",")
+  x = paste(name, level, hrs_to_complt, free_enrol, fin_aid, sep=",")
   cat(x, file="data.csv")
   print(link)
   print(x)
