0c983ff3a465309de8b45f14289755ce5ec6ef24
Showherda <showherda@gmail.com>
2022-01-13 01:45:28 +0800

    Update coursera_scraper.r

--- coursera_scraper.r
+++ coursera_scraper.r
@@ -7,6 +7,7 @@ page <- read_html(sitemap)
 
 course_links <- str_split(str_extract_all(page, "(?<=<loc>).*?(?=</loc>)")[[1]], " ")
 
+i <- 0
 for (link in course_links){
   html <- read_html(link)
   name <- html_element(html, css = ".banner-title-without--subtitle") %>% html_text()
@@ -16,7 +17,11 @@ for (link in course_links){
   free_enrol <- grepl("Enroll for Free", html)
   fin_aid <- grepl("Financial aid available", html)
   x = paste(name, level, hrs_to_complt, ratings, free_enrol, fin_aid, sep=",")
-  cat(x, file="data.csv")
+  
+  cat(x, file="data1.csv", append=T)
+  cat("\n", file="data1.csv", append=T)
   print(link)
   print(x)
+  i <- i+1
+  print(i)
 }
